064dd90633c2ba83e893f63f744d91ff37d5ad90
Jermaine Walters <walte1jj@gmail.com>
2022-03-24 16:22:39 -0400

    info appears from api  with input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#country-name");
-
-var countryInfo = function(country) {
-    var apiUrl = "https://restcountries.com/v3.1/name/{china, india, united states, indonesia, pakistan, brazil, nigeria, bangladesh, russia, mexico}";
-    
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-    });
-});
-};
+var countryContainerEl = document.querySelector("#country-container");
+var countrySearchTerm = document.querySelector("#country-search-term");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -17,10 +9,58 @@ var formSubmitHandler = function(event) {
     var countryName = nameInputEl.value.trim();
 
     if (countryName) {
-        countryInfo(countryName);
+        getCountryInfo(countryName);
         nameInputEl.value = "";
     } else {
         alert("Please enter a country name");
+    }
+};
+
+var getCountryInfo = function(china) {
+ //   var apiUrl = "https://restcountries.com/v3.1/name/{china, india, united states, indonesia, pakistan, brazil, nigeria, bangladesh, russia, mexico}";
+    var apiUrl = "https://restcountries.com/v3.1/name/{china}";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            console.log(response);
+        response.json().then(function(data) {
+            displayCountries(data, china);
+    });
+} else {
+    alert("Error: " + response.statusText);
+}
+})
+.catch(function(error) {
+    alert("Unable to connect to restcountries");
+});
+};
+
+var displayCountries = function(countries, searchTerm) {
+
+    if (countries.length === 0) {
+        countryContainerEl.textContent = "No countries found.";
+        return;
+    }
+
+    countrySearchTerm.textContent = searchTerm;
+
+    for (var i = 0; i < countries.length; i++) {
+        var countriesName = countries[i].name.common + " Current population: " + countries[i].population + " Region: " + countries[i].region + " Capital: " + countries[i].capital + " Continent: " + countries[i].continents;
+
+        var countriesEl = document.createElement("div");
+        countriesEl.classList = "list-item flex-row justify-space-between align-center";
+
+        var titleEl = document.createElement("span");
+        titleEl.textContent = countriesName;
+
+        countriesEl.appendChild(titleEl);
+
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        countriesEl.appendChild(statusEl);
+
+        countryContainerEl.appendChild(countriesEl);
     }
 };
 
