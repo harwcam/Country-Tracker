cd4b46587a4be5378e539c75480ef1cc75011daf
harwcam <84109744+harwcam@users.noreply.github.com>
2022-03-28 20:48:42 -0400

    Redesigned the getCountryData function into two separate functions, one for each API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 
-dropdownEl = document.querySelector("#submit-btn")
-capitalEl = document.querySelector("#capital span")
-regionEl = document.querySelector("#region span")
-coordinatesEl = document.querySelector('#coordinates')
+var dropdownEl = document.querySelector("#submit-btn")
+var capitalEl = document.querySelector("#capital span")
+var regionEl = document.querySelector("#region span")
+var coordinatesEl = document.querySelector('#coordinates span')
+var continentEl = document.querySelector('#continent span')
+var populationEl = document.querySelector('#population span')
 
 
 worldBankCountries = {
@@ -20,7 +22,7 @@ worldBankCountries = {
 
 
 
-var getCountryData = function(apiUrl) {
+var getWorldBankCountryData = function(apiUrl) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
@@ -44,25 +46,50 @@ var getCountryData = function(apiUrl) {
                 $(coordinatesEl.append(" " + coordinates))
                 $(regionEl.append( " " + region))
                 
-                console.log(capitalCity)
-                console.log(coordinates)
-                console.log(region)
             });
         } else{
-            alert("failure")
+            alert("Unable to connect to WorldBank API")
         }
     })
     .catch(function(error) {
-        console.log(error);
+        console.log("Error: " + error);
+    });
+};
+
+var getRestCountriesData = function(apiUrl) {
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                $(populationEl).text("Population:")
+                $(continentEl).text("Continent:")
+
+                var population = ""
+                var continent = ""
+                var population = data[0]["population"]
+                var continent = data[0]["continents"][0]
+
+                $(populationEl.append(" " + population))
+                $(continentEl.append(" " + continent))
+                
+                
+            });
+        } else{
+            alert("Unable to connect to WorldBank API")
+        }
+    })
+    .catch(function(error) {
+        console.log("Error: " + error);
     });
 };
 
 
 $('#countries-dropdown').change(function() {
    var countryKey = $('#countries-dropdown option:selected').text().trim()
-   var isoCode = worldBankCountries[countryKey]
-   var apiURL = `http://api.worldbank.org/v2/country/${isoCode}?format=json`
-   getCountryData(apiURL)
+   var wbIsoCode = worldBankCountries[countryKey]
+   var worldBankURL = `http://api.worldbank.org/v2/country/${wbIsoCode}?format=json`
+   var restCountriesURL = `https://restcountries.com/v3.1/alpha?codes=${wbIsoCode}`
+   getWorldBankCountryData(worldBankURL)
+   getRestCountriesData(restCountriesURL)
 })
 
 
